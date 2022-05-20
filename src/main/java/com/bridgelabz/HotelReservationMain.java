68c3458c911e0fb0d6df468d08e88bd3f9e0756c
praju-nikam <sawanprajakta84@gmail.com>
2022-05-20 15:19:42 +0530

    Wel-come to Hotel Reservation System

---
+++ src/main/java/com/bridgelabz/HotelReservationMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class HotelReservationMain
+{
+    public static void main(String[] args) {
+        System.out.println("*-*-*-*-*-*-*-*-*-*-Wel-Come to Hotel Reservation System -*-*-*-*-*-*-*-*-*-*");
+
+    }
+}
